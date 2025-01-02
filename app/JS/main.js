9ff2d5d90d45a0c7bc48151cb8b4c89bccf0aa4b
Judy <158211720+judyyan27@users.noreply.github.com>
2025-01-02 12:30:34 -0500

    add card and remove button works

--- app/JS/main.js
+++ app/JS/main.js
@@ -1,14 +1,13 @@
 import "../CSS/style.css";
 
 const DOMSelectors = {
-  container: document.querySelector(`.container-box`),
+  container: document.querySelector(`#container-box`),
 
   user_input: document.querySelector(`#user_input`),
-  input_button: document.querySelector(`#input_button`),
+  input_button: document.querySelector(`.input_button`),
 
   /* 
   activate_button: document.querySelector(`#`),
-  remove_button: document.querySelector(`#`), 
   clear_inputs: document.querySelector(`#`),*/
 };
 
@@ -19,4 +18,19 @@ DOMSelectors.input_button.addEventListener("click", function (event) {
   const input = DOMSelectors.user_input.value;
   inputs.push(input);
   console.log(inputs);
+
+  DOMSelectors.container.insertAdjacentHTML(
+    "beforeend",
+    `<div class="card">
+        <h1 class="card-title">${input}</h1>
+        <button type="remove" class="remove">Remove</button>
+      </div>`
+  );
+
+  const newcard = DOMSelectors.container.lastElementChild;
+  const removeButton = newcard.querySelector(".remove");
+
+  removeButton.addEventListener("click", function () {
+    newcard.remove();
+  });
 });
