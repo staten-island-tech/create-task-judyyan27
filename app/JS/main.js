37c21a637466dbc1a6aea96fd0b8b27f3e29599d
Judy <158211720+judyyan27@users.noreply.github.com>
2025-01-03 12:31:42 -0500

    finish the remove card button and add the activate random button --- work on algorithm next

--- app/JS/main.js
+++ app/JS/main.js
@@ -2,13 +2,13 @@ import "../CSS/style.css";
 
 const DOMSelectors = {
   container: document.querySelector(`#container-box`),
+  input_section: document.querySelector(`#input`),
 
   user_input: document.querySelector(`#user_input`),
   input_button: document.querySelector(`.input_button`),
 
-  /* 
-  activate_button: document.querySelector(`#`),
-  clear_inputs: document.querySelector(`#`),*/
+  activate_button: document.querySelector(`.activate_button`),
+   /* clearall_inputs: document.querySelector(`#`),*/
 };
 
 var inputs = [];
@@ -22,15 +22,28 @@ DOMSelectors.input_button.addEventListener("click", function (event) {
   DOMSelectors.container.insertAdjacentHTML(
     "beforeend",
     `<div class="card">
-        <h1 class="card-title">${input}</h1>
+        <h1 class="card-title">${input}</h1> 
         <button type="remove" class="remove">Remove</button>
       </div>`
   );
+
+  //DOMSelectors.user_input.textContent.remove();
 
   const newcard = DOMSelectors.container.lastElementChild;
   const removeButton = newcard.querySelector(".remove");
 
   removeButton.addEventListener("click", function () {
     newcard.remove();
+    inputs.splice(-1, 1);
+    console.log(inputs);
   });
 });
+
+// ALGORITHM HERE:
+
+DOMSelectors.activate_button.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  for
+
+}
